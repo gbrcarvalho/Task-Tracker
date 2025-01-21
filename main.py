ddb3df0d3cb5e7368ec8dbe71de143e87fdd5d1c
Gabriel Silva Carvalho <gccomputacao@gmail.com>
2025-01-21 00:52:35 -0300

    Atualizar main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import sys
 import datetime
 
-# status three possible values -> done, not done, in progress
-# bonus, add timestamp to the task 
-
-# Task Properties
-# Each task should have the following properties:
-
-# id: A unique identifier for the task
-# description: A short description of the task
-# status: The status of the task (todo, in-progress, done)
-# createdAt: The date and time when the task was created
-# updatedAt: The date and time when the task was last updated
-
 def write_task(id, description, status, createdAt, updatedAt):
     return f'{{\n"id": {id},\n"description": "{description}",\n"status": "{status}",\n"created at": "{createdAt}",\n"updated at": "{updatedAt}"\n}}\n'
 
